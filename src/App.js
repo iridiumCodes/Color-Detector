1b9c65d1ff817add3975f23291283ef7d6302e66
Iris <iridiumcodes@gmail.com>
2021-01-08 21:06:18 +0200

    component rendering with fragments

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkField from './components/ImageLinkField/ImageLinkField'
 import './App.css';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App" style={{display:'flex', justifyContent: 'space-between'}}>
-      <Logo />
-      <Navigation />
-      <ImageLinkField />
-      {/*<ColorDetection />*/} 
-    </div>
+    <>
+      <div className="App" style={{display:'flex', justifyContent: 'space-between'}}>
+        <Logo />
+        <Navigation />
+      </div>
+      <div>
+        <ImageLinkField />
+      </div>
+    </>
+
   );
 }
 
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import logo from './color-wheel.png';
 
 const Logo = () => {
   return (
-    <div className='ma3 m0' style={{display:'flex', justifyContent: 'flex-start'}}>
+    <div className='ma3 m0' style={{display:'flex'}}>
       <img src={logo} alt='Logo'/>
     </div>
   );
