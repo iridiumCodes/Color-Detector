58dd6b7b4742be12aafaf1346ce255c94c0bd24b
Iris <iridiumcodes@gmail.com>
2021-01-08 21:53:48 +0200

    basic styling field, button &bg

--- src/components/ImageLinkField/ImageLinkField.js
+++ src/components/ImageLinkField/ImageLinkField.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const ImageLinkField = () => {
   return (
-    <div></div>
+    <div className="center">
+      <h1>Discover your new color palette! Upload an image link below: </h1>
+      <div style={{display:"flex", width: "50%"}} className="pa4 br3 shadow-5">
+
+        <input type="text" className="w-two-thirds pa2"/>
+        <button className="button w-third grow link f4 pv2 light-purple">Submit</button>
+      </div>
+    </div>
   );
 
 }
