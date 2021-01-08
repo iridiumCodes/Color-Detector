627761a85a543b04aac792e3b8e3b7612a67bacf
Iris <iridiumcodes@gmail.com>
2021-01-08 22:15:35 +0200

    background for input section

--- src/components/ImageLinkField/ImageLinkField.js
+++ src/components/ImageLinkField/ImageLinkField.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './ImageLinkField.css';
 
 const ImageLinkField = () => {
   return (
     <div className="center">
       <h1>Discover your new color palette! Upload an image link below: </h1>
-      <div style={{display:"flex", width: "50%"}} className="pa4 br3 shadow-5">
-
+      <div style={{display:"flex", width: "50%"}} className="bg-gradient pa4 br3 shadow-5git">
         <input type="text" className="w-two-thirds pa2"/>
         <button className="button w-third grow link f4 pv2 light-purple">Submit</button>
       </div>
