84a8c6e93d81543370cab418d2d751d3572c168d
Iris <iridiumcodes@gmail.com>
2021-01-10 00:01:48 +0200

    button text changed

--- src/components/ImageLinkField/ImageLinkField.js
+++ src/components/ImageLinkField/ImageLinkField.js
@@ -7,7 +7,7 @@ const ImageLinkField = ({onInputChange}) => {
       <h1>Discover your new color palette! Upload an image link below: </h1>
       <div style={{display:"flex", width: "75%"}} className="bg-gradient pa4 ma4 br3 shadow-5">
         <input type="text" className="w-two-thirds pa2" onChange={onInputChange}/>
-        <button className="button w-third grow link f4 pv2 light-purple tc">Submit</button>
+        <button className="button w-third grow link f4 pv2 light-purple tc">Detect</button>
       </div>
     </div>
   );
