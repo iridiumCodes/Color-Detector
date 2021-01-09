84a3bd96cd886192fc373e3bb1b226b6d2be8cc6
Iris <iridiumcodes@gmail.com>
2021-01-09 23:59:52 +0200

    centering h1

--- src/components/ImageLinkField/ImageLinkField.js
+++ src/components/ImageLinkField/ImageLinkField.js
@@ -3,7 +3,7 @@ import './ImageLinkField.css';
 
 const ImageLinkField = ({onInputChange}) => {
   return (
-    <div className="center">
+    <div className="center tc">
       <h1>Discover your new color palette! Upload an image link below: </h1>
       <div style={{display:"flex", width: "75%"}} className="bg-gradient pa4 ma4 br3 shadow-5">
         <input type="text" className="w-two-thirds pa2" onChange={onInputChange}/>
