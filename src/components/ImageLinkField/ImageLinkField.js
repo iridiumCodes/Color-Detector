186e7610a567a0828220e3d46ce628509ada9b1b
Iris <iridiumcodes@gmail.com>
2021-01-09 23:58:28 +0200

    button text center

--- src/components/ImageLinkField/ImageLinkField.js
+++ src/components/ImageLinkField/ImageLinkField.js
@@ -5,9 +5,9 @@ const ImageLinkField = ({onInputChange}) => {
   return (
     <div className="center">
       <h1>Discover your new color palette! Upload an image link below: </h1>
-      <div style={{display:"flex", width: "50%"}} className="bg-gradient pa4 ma4 br3 shadow-5">
+      <div style={{display:"flex", width: "75%"}} className="bg-gradient pa4 ma4 br3 shadow-5">
         <input type="text" className="w-two-thirds pa2" onChange={onInputChange}/>
-        <button className="button w-third grow link f4 pv2 light-purple">Submit</button>
+        <button className="button w-third grow link f4 pv2 light-purple tc">Submit</button>
       </div>
     </div>
   );
