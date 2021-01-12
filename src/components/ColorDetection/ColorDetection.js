db291c55e55469e351e535558f0792e352d7fad8
Iris <iridiumcodes@gmail.com>
2021-01-12 22:28:37 +0200

    restrict the size of the image displayed for consistency

--- src/components/ColorDetection/ColorDetection.js
+++ src/components/ColorDetection/ColorDetection.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ColorDetection = ({ imageUrl }) => {  /*pass image URL as props in the ColorDetection Component */
   return (
-    <div className='center'>
-      <img src={imageUrl} alt=""/>
+    <div className='center ma'>
+      <div className= 'mt2'>
+        <img src={imageUrl} alt="" width='600px' height='auto' />
+      </div>
     </div>
 
   );
