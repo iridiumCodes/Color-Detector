4d0615a99b74f79c2fe8610187ea1c0cae42ea55
Iris <iridiumcodes@gmail.com>
2021-01-13 23:21:47 +0200

    fixed all centering issues and responsive buttong

--- src/components/ImageLinkField/ImageLinkField.js
+++ src/components/ImageLinkField/ImageLinkField.js
@@ -3,13 +3,15 @@ import './ImageLinkField.css';
 
 const ImageLinkField = ({onInputChange, onButtonDetect}) => {
   return (
-    <div className="center tc">
-      <h1>Discover your new color palette! Upload an image link below: </h1>
-      <div style={{display:"flex", width: "75%"}} className="bg-gradient pa4 ma4 br3 shadow-5">
-        <input type="text" className="w-two-thirds pa2" onChange={onInputChange}/>
-        <button className="button w-third grow link f4 pv2 light-purple tc" onClick={onButtonDetect}>Detect</button>
+    <container className="centerVertical">
+      <div className="centerVertical tc">
+        <h1>Discover your new color palette! Upload an image link below: </h1>
+        <div className="w-two-thirds bg-gradient pa4 ma4 br3 shadow-5 center">
+          <input type="text" className="w-two-thirds pa2" onChange={onInputChange}/>
+          <button className="w-third grow link f4 pv2 light-purple tc" onClick={onButtonDetect}>Detect</button>
+        </div>
       </div>
-    </div>
+    </container>
   );
 
 }
