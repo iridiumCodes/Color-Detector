748a60933a72212590d9bb0d4d2c151a72397141
Iris <iridiumcodes@gmail.com>
2021-01-14 00:32:43 +0200

    changed logo size

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,8 @@ import logo from './color-wheel.png';
 
 const Logo = () => {
   return (
-    <div className='ma3 m0 grow' style={{display:'flex'}}>
-      <img src={logo} alt='Logo'/>
+    <div className='logo ma3 m0 grow'>
+      <img src={logo} alt='Logo' style={{width: '60%'}}/>
     </div>
   );
 
