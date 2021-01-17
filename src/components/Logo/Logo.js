38d66e63d30c59808291f488ca51c4a6f5677240
Iris <iridiumcodes@gmail.com>
2021-01-17 17:27:07 +0200

    cleanup

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,8 +3,8 @@ import logo from './color-wheel.png';
 
 const Logo = () => {
   return (
-    <div className='logo ma3 m0 grow center'>
-      <img src={logo} alt='Logo' style={{width: '60%'}}/>
+    <div className='ma3 m0 grow center'>
+      <img src={logo} alt='Logo' style={{width: '75%'}}/>
     </div>
   );
 
