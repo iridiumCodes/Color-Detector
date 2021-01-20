b3542ef08063f4926f52e08cb2206e4425c29091
Iris <iridiumcodes@gmail.com>
2021-01-20 23:56:36 +0200

    signin smart component set state

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
-const Signin = ({ onRouteChange }) => {
-  return (
-    <main className="pa4 black-80">
+class Signin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    }
+  }
+
+  onEmailChange = (event) => {
+    this.setState ({signInEmail : event.target.value})
+  }
+
+  onPasswordChange = (event) => {
+    this.setState ({signInPassword : event.target.value})
+  }
+
+  render () {
+    const { onRouteChange } =this.props;
+    return ( 
+      <main className="pa4 black-80">
       <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center pa3 shadow-5">
         <div className="measure">
           <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
-            <legend className="f4 fw6 ph0 mh0 center">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
-            </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
-            </div>
-            <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
+          <legend className="f4 fw6 ph0 mh0 center">Sign In</legend>
+          <div className="mt3">
+            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+          </div>
+          <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
           </fieldset>
           <div className="">
             <input onClick = { () => onRouteChange('home') } className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
@@ -26,8 +44,9 @@ const Signin = ({ onRouteChange }) => {
           </div>
         </div>
       </article>
-  </main>
-  )
+    </main>
+    )
+  }
 }
 
 export default Signin;
