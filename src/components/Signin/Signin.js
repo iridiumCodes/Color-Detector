c24c9513a5c4f9884743b0894abb74fa624c7d23
Iris <iridiumcodes@gmail.com>
2021-01-21 21:53:44 +0200

    signin for specific username and password only to route change to home

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,6 +17,22 @@ class Signin extends React.Component {
     this.setState ({signInPassword : event.target.value})
   }
 
+  onSubmitSignin = () => {
+  fetch('http://localhost:3000/signin', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      email: this.state.signInEmail,
+      password: this.state.signInPassword
+    })
+  })
+    .then(response => response.json())
+    .then( data => {
+      if (data === 'success') {
+      this.props.onRouteChange('home')
+    }})
+  }
+
   render () {
     const { onRouteChange } =this.props;
     return ( 
@@ -27,16 +43,16 @@ class Signin extends React.Component {
           <legend className="f4 fw6 ph0 mh0 center">Sign In</legend>
           <div className="mt3">
             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+            <input onChange = {this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
           </div>
           <div className="mv3">
             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+            <input onChange = {this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
           </div>
           <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
           </fieldset>
           <div className="">
-            <input onClick = { () => onRouteChange('home') } className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
+            <input onClick = {this.onSubmitSignin } className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
           </div>
           <div className="lh-copy mt3">
             <p onClick = { () => onRouteChange('register') } href="#0" className="f6 link dim black db pointer">Register</p>
