882dfaa7605886723ba09af64f76692c8ad6387e
Iris <iridiumcodes@gmail.com>
2021-02-15 16:13:15 +0200

    correct conditional rendering for swatches component

--- src/components/Swatches/Swatches.js
+++ src/components/Swatches/Swatches.js
@@ -4,35 +4,32 @@ const Swatches = ({ colors }) => {
   console.log(colors);
   // const entries = Object.entries(colors);
   // console.log(entries);
-  switch (!colors) {
-    case true:
-      return (
-        <>
-          <h2>Colors Detected:</h2>
-          {colors.map((color) => (
-            // <div className="center pa2">
-            <>
-              <div className="container">
-                <div
-                  key={color.value}
-                  style={{
-                    height: '40px',
-                    width: '80px',
-                    backgroundColor: color.raw_hex,
-                  }}
-                ></div>
-                <div>
-                  <p>{color.value}</p>
-                </div>
+  if (colors.length) {
+    return (
+      <>
+        <h2>Colors Detected:</h2>
+        {colors.map((color) => (
+          // <div className="center pa2">
+          <>
+            <div className="container">
+              <div
+                key={color.value}
+                style={{
+                  height: '40px',
+                  width: '80px',
+                  backgroundColor: color.raw_hex,
+                }}
+              ></div>
+              <div>
+                <p>{color.value}</p>
               </div>
-            </>
-          ))}
-        </>
-      );
-      break;
-    case false:
-      return null;
-      break;
+            </div>
+          </>
+        ))}
+      </>
+    );
+  } else {
+    return null;
   }
 };
 // colorsArray.sort((a, b) =>
